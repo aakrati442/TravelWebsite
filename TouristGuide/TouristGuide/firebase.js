862d337e0050de50f4feb03438675303c2024a8d
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-04-26 10:34:45 +0530

    Add files via upload

---
+++ TouristGuide/TouristGuide/firebase.js
@@ -0,0 +1,32 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+  
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyB6gA__CC4CsvNGKepjBrwofGkM1N-PHIc",
+//     authDomain: "touristguide-3d608.firebaseapp.com",
+//     databaseURL: "https://touristguide-3d608-default-rtdb.firebaseio.com/",
+//     projectId: "touristguide-3d608",
+//     storageBucket: "touristguide-3d608.appspot.com",
+//     messagingSenderId: "793438750718",
+//     appId: "1:793438750718:web:05524f9a9b6954b34662af"
+//   };
+
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6aC9fjHgMCn6Td0lOY1q82D9vnpznF-Y",
+  authDomain: "book-6f495.firebaseapp.com",
+  databaseURL: "https://book-6f495-default-rtdb.firebaseio.com",
+  projectId: "book-6f495",
+  storageBucket: "book-6f495.appspot.com",
+  messagingSenderId: "226429765422",
+  appId: "1:226429765422:web:4826f3587ef18755071cbf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
